769b9052ed8786371157fb089bbd3a57ef4371fa
Keith Ratner <keith.ratner@gmail.com>
2019-12-10 14:15:45 -0800

    Fixed ColorSwatches unit test

--- src/components/__tests__/ColorSwatches.test.js
+++ src/components/__tests__/ColorSwatches.test.js
@@ -4,7 +4,20 @@ import ColorSwatches from "../ColorSwatches";
 
 describe("ColorSwatches", () => {
   it("renders correctly", () => {
-    const wrapper = shallow(<ColorSwatches />);
+    const wrapper = shallow(
+      <ColorSwatches
+        layout="vertical"
+        shape="circle"
+        size="1em"
+        colors={["red", "green", "blue"]}
+        spacing=".25em"
+        /*
+        onSwatchClick={_color => {
+          this.handleColorSwatchClick(this, _color);
+        }}
+        */
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
